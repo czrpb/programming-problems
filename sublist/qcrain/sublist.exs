1a5563db31603cb8ce258ff44273046d90009bb7
czrpb <czrpb@yahoo.com>
2022-04-15 10:42:48 -0700

    1st implementation

---
+++ sublist/qcrain/sublist.exs
@@ -0,0 +1,19 @@
+defmodule Sublist do
+  def sublist?(_, []), do: :missing
+
+  def sublist?(l, l), do: :equal
+
+  def sublist?(l1, l2) do
+    if List.starts_with?(l2, l1) do
+      :sublist
+    else
+      sublist?(l1, tl(l2))
+    end
+  end
+end
+
+[l1, l2 | _] = System.argv()
+|> Enum.map(&String.to_charlist/1)
+
+Sublist.sublist?(l1, l2)
+|> inspect |> IO.puts
