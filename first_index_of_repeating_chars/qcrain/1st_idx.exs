2076abc7559aadd84599c7707fa4c01a13d487aa
czrpb <czrpb@yahoo.com>
2022-04-14 22:40:05 -0700

    2nd version using Enum library

--- first_index_of_repeating_chars/qcrain/1st_idx.exs
+++ first_index_of_repeating_chars/qcrain/1st_idx.exs
@@ -5,17 +5,26 @@ defmodule FirstIndexOfRepeatingChars do
 
   def first(<<>>, _, result), do: Enum.reverse(result)
 
-  def first(<<c, remaining::binary>>, i, [{c,_}|_]=result) do
-    first(remaining, i+1, result)
+  def first(<<c, remaining::binary>>, i, [{c, _} | _] = result) do
+    first(remaining, i + 1, result)
   end
 
   def first(<<c, remaining::binary>>, i, result) do
-    first(remaining, i+1, [{c, i}|result])
+    first(remaining, i + 1, [{c, i} | result])
   end
-
 end
 
-[s|_] = System.argv()
+defmodule FirstIndexOfRepeatingChars2 do
+  def first(s) do
+    s
+    |> String.to_charlist()
+    |> Enum.with_index()
+    |> Enum.dedup_by(fn {e, _} -> e end)
+  end
+end
 
-FirstIndexOfRepeatingChars.first(s)
-|> inspect |> IO.puts
+[s | _] = System.argv()
+
+FirstIndexOfRepeatingChars.first(s) |> inspect |> IO.puts()
+
+FirstIndexOfRepeatingChars2.first(s) |> inspect |> IO.puts()
